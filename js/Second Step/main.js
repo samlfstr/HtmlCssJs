dad78423aefbe2bc819beb6af75e219f31dadb7b
Aaron <samlfstr@gmail.com>
2021-04-26 22:25:13 +0300

    Update

--- js/Second Step/main.js
+++ js/Second Step/main.js
@@ -23,8 +23,33 @@ if(car_type.indexOf('es') !== -1){
     console.log('False');
 }
 
-// Select single character
+// single characters
 let life = 'is a bitch';
+console.log(life[0] + life[1] + life[3]);
+
+// reverse the order
+let i = life.length;
+for (i; i > 0; i--) {
+    console.log(life[life.length-i]);
+}
+
+// mixing slice() and indexOf()
+let index = life.indexOf('a');
+// after the 'a' included
+console.log(life.slice(index));
+// before the 'a' excluded
+console.log(life.slice(0,index));
+
+// lowercase
+console.log(life.toLocaleLowerCase());
+// uppercase
+console.log(life.toUpperCase());
+
+//make first letter upper case
+
+
+
+
 
 
 
