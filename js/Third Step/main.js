f1788963d75fe1e2d515ddc28bff05558157a7f9
Aaron <samlfstr@gmail.com>
2021-04-28 20:54:12 +0300

    update

--- js/Third Step/main.js
+++ js/Third Step/main.js
@@ -1,1 +1,35 @@
-// array methods
+// add/remove array methods
+let currency = ['dollar','euro','lira'];
+
+// add to end
+currency.push('yen');
+console.log(currency);
+// remove from the end
+currency.pop();
+console.log(currency);
+
+// add to the beginning
+currency.shift();
+console.log(currency);
+// remove from the beginning
+currency.unshift('CFA');
+console.log(currency)
+
+// using the splice method
+let numbers = ['a','b','c','d','e'];
+numbers.splice(0, 1,'A');
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
