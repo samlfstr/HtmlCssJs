21f28df6f812692d24df682cca5c0b3b58b54d3a
Aaron <samlfstr@gmail.com>
2021-05-29 13:13:07 +0300

    update

---
+++ js/Exercice/Generic/objects/objects.js
@@ -0,0 +1,32 @@
+// multi dimentional objects
+
+let questions = [
+    {
+
+        Question : 'What is your name ?',
+        Answer : 'Samuel'
+    },
+    {
+        Question : 'Where are you from ?',
+        Answer : 'Jerusalem'
+    },
+    {
+        Question : 'What is your profession ?',
+        Answer : 'Programmer'
+    },
+
+];
+
+/*
+for of then for in
+question represents each question
+question key represents keys of each question block
+and then because you are inside the for in loop
+you can either call properties / keys or property values / values
+*/
+for (const question of questions) {
+    for (const questionKey in question) {
+        document.write(`${questionKey} : `+ question[questionKey] + '<br>');
+        document.write('<br>');
+    }
+}
