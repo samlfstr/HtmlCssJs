cabe03ba573644e5a36dca02a618da8daecebac5
Aaron <samlfstr@gmail.com>
2021-07-13 22:24:02 +0300

    update

--- js/Second-Step/Jquery/Plugins/Pageload/main.js
+++ js/Second-Step/Jquery/Plugins/Pageload/main.js
@@ -6,3 +6,36 @@ $(".animsition").animsition({
     inDuration: 500,
     outDuration: 300
 });
+
+// Selection
+let header_select = $('.header').sticky();
+
+header_select.on(
+    'sticky-start',
+    () => {
+        $('.description').html('<h1 class="description">We build <strong> Greate </strong> apps.</h1>');
+    }
+);
+
+header_select.on(
+    'sticky-end',
+    () => {
+        $('.description').html('<h1 class="description">We build apps</h1>');
+    }
+);
+
+var emailLink = $('<a href="mailto:help@teamtreehouse.com">Email us</a>');
+$(() => {
+    $("h5").sticky({
+        topSpacing: 64
+    }).on(
+        'sticky-start',
+        () => {
+            $(this).append(" ").append(emailLink);
+        }).on(
+        'sticky-end',
+        () => {
+            emailLink.remove();
+        });
+});
+
