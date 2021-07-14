8619c7b18d4b5e594efa75102a84e8edc97479cc
Aaron <samlfstr@gmail.com>
2021-07-14 17:24:21 +0300

    update

--- js/Second-Step/Jquery/Plugins/Pageload/main.js
+++ js/Second-Step/Jquery/Plugins/Pageload/main.js
@@ -10,32 +10,38 @@ $(".animsition").animsition({
 // Selection
 let header_select = $('.header').sticky();
 
-header_select.on(
-    'sticky-start',
-    () => {
-        $('.description').html('<h1 class="description">We build <strong> Greate </strong> apps.</h1>');
-    }
-);
-
-header_select.on(
-    'sticky-end',
-    () => {
-        $('.description').html('<h1 class="description">We build apps</h1>');
-    }
-);
-
 var emailLink = $('<a href="mailto:help@teamtreehouse.com">Email us</a>');
-$(() => {
-    $("h5").sticky({
-        topSpacing: 64
-    }).on(
+
+$(document).ready(function () {
+    $("h5").sticky({topSpacing: 60})
+        .on('sticky-start', function () {
+            $(this).append(" ").append(emailLink);
+        })
+        .on('sticky-end', function () {
+            emailLink.remove();
+        });
+
+    header_select.on(
         'sticky-start',
         () => {
-            $(this).append(" ").append(emailLink);
+            $('.description').html('<h1 class="description">We build <strong> Greate </strong> apps.</h1>');
         }).on(
         'sticky-end',
         () => {
-            emailLink.remove();
-        });
+            $('.description').html('<h1 class="description">We build apps</h1>');
+        }
+    );
+
+    $('.slicky').slick({
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1,
+        arrowRight : true,
+        arrowLeft : true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
 });
 
